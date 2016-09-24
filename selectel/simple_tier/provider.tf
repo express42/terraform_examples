efc9cb3ceba59dfe92e49d69622c435bcb6de15b
Artem Starostenko <artemstarostenko65@gmail.com>
2016-09-24 16:32:01 +0300

    Create a default file for input vars and move sensitive info into it

--- selectel/simple_tier/provider.tf
+++ selectel/simple_tier/provider.tf
@@ -1,8 +1,8 @@
 provider "openstack" {
-    user_name  = "${var.selectel["user_name"]}"
-    domain_name = "${var.selectel["domain_name"]}"
-    tenant_name = "${var.selectel["tenant_name"]}"
-    tenant_id = "${var.selectel["tenant_id"]}"
-    password  = "${var.selectel["password"]}"
+    user_name  = "${var.selectel_user_name}"
+    domain_name = "${var.selectel_domain_name}"
+    tenant_name = "${var.selectel_tenant_name}"
+    tenant_id = "${var.selectel_tenant_id}"
+    password  = "${var.selectel_password}"
     auth_url  = "https://api.selvpc.ru/identity/v3"
 }
